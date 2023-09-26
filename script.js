6b554a933690cc71c7a8cdadc2105fe734cc6c21
BrunoAmadei <brunoamadei98@gmail.com>
2023-09-26 09:55:50 -0300

    update teste

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const catalog = document.querySelector('.catalog');
+const book = document.querySelector('.book');
 const pages = document.querySelectorAll('.page');
 const prevButton = document.getElementById('prevPage');
 const nextButton = document.getElementById('nextPage');
 let currentPage = 0;
 
 function flipPage(direction) {
-    if (direction === 'next' && currentPage < pages.length - 1) {
-        currentPage++;
-    } else if (direction === 'prev' && currentPage > 0) {
-        currentPage--;
+    const nextPage = direction === 'next' ? currentPage + 1 : currentPage - 1;
+    if (nextPage >= 0 && nextPage < pages.length) {
+        pages[currentPage].style.transform = 'rotateY(-180deg)';
+        pages[nextPage].style.transform = 'rotateY(0deg)';
+        currentPage = nextPage;
     }
-
-    catalog.style.transform = `rotateY(${currentPage * -180}deg)`;
 }
 
 prevButton.addEventListener('click', () => flipPage('prev'));
